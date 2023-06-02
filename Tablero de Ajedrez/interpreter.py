2ea1b272fb476cf4794a417af8693062c9be574f
OwerFrankLopezArela <121266700+OwerFrankLopezArela@users.noreply.github.com>
2023-06-02 11:49:50 -0500

    Creamos la funcion para el dibujo de cada pieza

--- Tablero de Ajedrez/interpreter.py
+++ Tablero de Ajedrez/interpreter.py
@@ -8,3 +8,28 @@ def parseLine(DISPLAY, y, s):
   for c in s:
     pygame.draw.line(DISPLAY, color[c], (x, y), (x, y))
     x += 1
+
+# Función principal para dibujar la imagen
+def draw(picture):
+  try:
+    img = picture.img
+  except:
+    img = picture
+  pygame.init()
+
+  # Crear una ventana de visualización de tamaño 640x480
+  DISPLAY=pygame.display.set_mode((640, 480))
+  DISPLAY.fill(BLUE)
+
+  n = len(img)
+  # Iterar sobre las líneas de la imagen y dibujarlas en la ventana
+  for i in range(0, n):
+    parseLine(DISPLAY, i, img[i])
+
+  while True:
+    # Manejar eventos de pygame
+    for event in pygame.event.get():
+      if event.type==QUIT:
+        pygame.quit()
+        #sys.exit()
+    pygame.display.update()
